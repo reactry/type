18f13833235344af0dd3718b7609e1c3c114aeeb
Ankur Seth <iankurseth@gmail.com>
2022-02-10 19:51:16 +0530

    Displaying 500 words on homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="px-8 py-20 bg-slate-400 text-2xl text-white">
-          Hello, Type!
-        </p>
-      </header>
-    </div>
-  );
+import json500 from './data/500.json';
+const words = json500.words;
+
+
+
+function App () {
+
+	let wordItems = words.map((w, i) => {
+		return (
+			<div key={i} className="px-4 py-2 bg-purple-400 rounded-full">{w}</div>
+		);
+	});
+
+	return (
+		<div className="App">
+			<main className="bg-purple-200 py-32">
+				<div className="flex flex-wrap max-w-5xl m-auto gap-x-2 gap-y-4">
+					{wordItems}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
